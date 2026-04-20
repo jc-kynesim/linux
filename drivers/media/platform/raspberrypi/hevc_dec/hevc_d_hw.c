026b0e18f835aa4c6ef9f534555d7ae7f3f76962
John Cox <jc@kynesim.co.uk>
2026-04-20 18:47:14 +0100

    hevc_d: Fix start_clock some more (refs)

--- drivers/media/platform/raspberrypi/hevc_dec/hevc_d_hw.c
+++ drivers/media/platform/raspberrypi/hevc_dec/hevc_d_hw.c
@@ -337,7 +337,8 @@ int hevc_d_hw_start_clock(struct hevc_d_dev *dev)
 
 	rv = clk_prepare_enable(dev->clock);
 	if (rv) {
-		dev_err(dev->dev, "Failed to enable clock\n");
+		--dev->clk_refs;
+	        dev_err(dev->dev, "Failed to enable clock\n");
 		goto done;
 	}
 	irq_clear_enable(dev);
